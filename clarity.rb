d5e4955d0f5a80b20d3e2f64eea9dfb0664758b4
Steven Herod <steven.herod@inthecloud.com.au>
2012-04-18 09:26:24 +1000

    Updated Readme and added #! for commandline

--- clarity.rb
+++ clarity.rb
@@ -1,4 +1,5 @@
-# Copyright (c) 2012 Steven Herod, http://github.com/sherod/clarity
+#!/usr/bin/ruby
+# Copyright (c) 2012 Steven Herod, http://github.com/sherod/Clarity
 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
